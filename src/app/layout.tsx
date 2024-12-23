32d99cf7358c6d846eddfe3ec1e330a673cbe0c3
Nana Mohamadi <adilnana78@gmail.com>
2024-12-24 00:00:28 +0100

    Configure redux and react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ReduxProvider } from "@/redux/provider";
+import { ReactQueryProvider } from "@/react-query/provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReduxProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </ReduxProvider>
+        <Toaster />
       </body>
     </html>
   );
